a3980fbb1aa0b5da55c0daebad820ff0ec84b96f
Thibault <laurens.thibault@gmail.com>
2012-03-25 03:47:36 +0100

    UI V 0.41

--- RMM/RMM.Phone/ViewModel/CreateCategoryViewModel.cs
+++ RMM/RMM.Phone/ViewModel/CreateCategoryViewModel.cs
@@ -1,11 +1,30 @@
 ﻿using GalaSoft.MvvmLight;
+using RMM.Phone.ViewData.Account;
+using GalaSoft.MvvmLight.Command;
 
 namespace RMM.Phone.ViewModel
 {
     public class CreateCategoryViewModel : ViewModelBase
     {
+        public CategoryViewData Category { get; set; }
+        public RelayCommand SaveCommand { get; set; }
+        public RelayCommand CancelCommand { get; set; }
+
         public CreateCategoryViewModel()
         {
+            SaveCommand = new RelayCommand(() => HandleCreateTaskSelected());
+            CancelCommand = new RelayCommand(() => HandleCancelTaskSelected());
+        }
+
+        void HandleCreateTaskSelected()
+        {
+            //LANCER LA CREATION ICI
+        }
+
+        void HandleCancelTaskSelected()
+        {
+            var rootFrame = (App.Current as App).RootFrame;
+            rootFrame.Navigate(new System.Uri("/MainPage.xaml", System.UriKind.Relative));
         }
     }
 }
